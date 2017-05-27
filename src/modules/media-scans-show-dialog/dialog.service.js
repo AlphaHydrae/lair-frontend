ee6909758c9f6110eb5bab7b31971fb12cb05c9a
Simon Oulevay <simon.oulevay@gmail.com>
2017-05-27 17:49:10 +0200

    Media scan file list

--- src/modules/media-scans-show-dialog/dialog.service.js
+++ src/modules/media-scans-show-dialog/dialog.service.js
@@ -21,16 +21,30 @@ angular.module('lair').factory('showMediaScanDialog', function($uibModal) {
   return service;
 }).controller('ShowMediaScanDialogCtrl', function(api, mediaScans, $uibModalInstance, $scope) {
 
-  if (!$scope.mediaScan && $scope.mediaScanId) {
-    fetchMediaScan($scope.mediaScanId);
+  var mediaScanId = _.get($scope, 'mediaScan.id', $scope.mediaScanId);
+  if (!$scope.mediaScan && mediaScanId) {
+    fetchMediaScan(mediaScanId);
   }
 
+  var page = 0;
+  var pageSize = 100;
+  $scope.changes = [];
+  $scope.currentPage = 1;
+  $scope.changesPageLinks = [];
   $scope.scanDuration = mediaScans.getDuration;
+
+  $scope.$watch('mediaScan.id', function(value) {
+    if (value && $scope.mediaScan.changedFilesCount >= 1) {
+      showChangesPage($scope.currentPage);
+    }
+  });
+
+  $scope.showChangesPage = showChangesPage;
 
   $scope.retry = function() {
     api({
       method: 'POST',
-      url: '/media/scans/' + $scope.mediaScan.id + '/retry'
+      url: '/media/scans/' + mediaScanId + '/retry'
     }).then(function() {
       $uibModalInstance.dismiss();
     });
@@ -45,5 +59,69 @@ angular.module('lair').factory('showMediaScanDialog', function($uibModal) {
     }).then(function(res) {
       $scope.mediaScan = res.data;
     });
+  }
+
+  function showChangesPage(page) {
+    if ($scope.changes.length && page == $scope.currentPage) {
+      return;
+    }
+
+    $scope.currentPage = page;
+
+    api({
+      url: '/media/scans/' + mediaScanId + '/files',
+      params: {
+        include: 'data',
+        start: (page - 1) * pageSize,
+        number: pageSize
+      }
+    }).then(function(res) {
+      $scope.changes = res.data;
+      updateChangesPageLinks();
+    });
+  }
+
+  function updateChangesPageLinks() {
+
+    var numberOfPages = Math.ceil($scope.mediaScan.changedFilesCount / pageSize);
+    if (numberOfPages <= 1) {
+      $scope.changesPageLinks = [];
+      delete $scope.lastPage;
+    } else if (numberOfPages <= 5) {
+      $scope.changesPageLinks = _.map(new Array(numberOfPages), function(i) {
+        return i + 1;
+      });
+      $scope.lastPage = numberOfPages;
+    } else {
+
+      $scope.changesPageLinks = [ $scope.currentPage ];
+      addPageLinks($scope.changesPageLinks, Math.max(1, $scope.currentPage - 2));
+      addPageLinks($scope.changesPageLinks, Math.min(numberOfPages, $scope.currentPage + 2));
+      addPageLinks($scope.changesPageLinks, Math.max(1, $scope.currentPage - 4));
+      addPageLinks($scope.changesPageLinks, Math.min(numberOfPages, $scope.currentPage + 4));
+
+      $scope.lastPage = numberOfPages;
+    }
+  }
+
+  function addPageLinks(links, limit) {
+    if (links.length >= 5) {
+      return;
+    }
+
+    var direction = 1;
+    if (limit <= links[0]) {
+      direction = -1;
+    } else if (limit < links[links.length - 1]) {
+      throw new Error('Limit ' + limit + ' inconsistent with links ' + JSON.stringify(links));
+    }
+
+    while (links.length < 5 && direction > 0 ? (limit - links[links.length - 1] > 0) : (links[0] - limit > 0)) {
+      if (direction > 0) {
+        links.push(links[links.length - 1] + 1);
+      } else {
+        links.unshift(links[0] - 1);
+      }
+    }
   }
 });
